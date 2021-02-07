909c59ac20f3dbdc944ec960fd385957aea1d2a6
Tako <tako20020218@gmail.com>
2021-02-08 05:13:13 +0900

    add_omomiUnionFind
    第二,三週の活動

--- library.py
+++ library.py
@@ -51,3 +51,60 @@ n = int(input())
 par = [-1] * (4*10**6)
 MAX = 0
 '''
+  # 重み付きUnionFind-----------------------------------
+'''
+# xの根を求める
+def find(x):
+    if par[x] < 0:
+        return x
+    else:
+        px = find(par[x])
+        wei[x] += wei[par[x]]
+        par[x] = px
+        return px
+
+
+# xの根から距離
+def weight(x):
+    find(x)
+    return wei[x]
+
+
+# w[y]=w[x]+wとなるようにxとyを併合
+def unite(x, y, w):
+    w += wei[x] - wei[y]
+    x = find(x)
+    y = find(y)
+
+    if x == y:
+        return False
+    else:
+        # sizeの大きいほうがx
+        if par[x] > par[y]:
+            x, y = y, x
+            w = -w
+        par[x] += par[y]
+        par[y] = x
+        wei[y] = w
+        return True
+
+
+# xとyが同じ集合に属するかの判定
+def same(x, y):
+    return find(x) == find(y)
+
+
+# xが属する集合の個数
+def size(x):
+    return -par[find(x)]
+
+
+# x,yが同じ集合に属するときのwei[y]-wei[x]
+def diff(x, y):
+    return weight(y) - weight(x)
+'''
+#nは要素数
+'''
+n = int(input())
+par = [-1] * n
+'''
